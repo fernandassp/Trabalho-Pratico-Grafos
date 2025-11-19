59c02bfec58724c08615a45f662f7b3b77b95dc5
fernandassp <fernandassp1907@gmail.com>
2025-11-19 15:51:50 -0300

    estrutura básica do menu principal

---
+++ TP_Grafos/GrafoDirecionado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Grafos
+{
+    internal class GrafoDirecionado
+    {
+        // lista ou matriz de adjacência de acordo com a densidade
+    }
+}
--- TP_Grafos/Program.cs
+++ TP_Grafos/Program.cs
@@ -2,9 +2,61 @@
 {
     internal class Program
     {
+        static void ExibirMenu()
+        {
+            Console.WriteLine("\t\t\t--- MÁXIMA LOGÍSTICA S.A. ---\n");
+            Console.WriteLine("\tDigite o número correspondente à opção desejada:");
+            Console.WriteLine("1- Roteamento de menor custo\n2- Capacidade Máxima de Escoamento\n3- Expansão da Rede de Comunicação\n4- Agendamento de Manutenções sem Conflito\n5- Rota Única de Inspeção\n6- Sair");
+        }
+
+        static void LerDadosGrafo() // colocar retorno certo depois
+        {
+            // implementar para ler dados de um arquivo e a partir disso montar um grafo 
+        }
+
+        // método para calcular densidade fica aqui ou na classe grafo? outra?
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World?");
+            //criar a estrutura de dados que armazena a rede
+            int opc;
+
+            // arquivo de logs que sera usado para escrever os resultados depois
+            StreamWriter arquivoLog = new StreamWriter("log.txt", false);
+
+            do
+            {
+                ExibirMenu();
+                opc = int.Parse(Console.ReadLine());
+
+                switch (opc)
+                {
+                    case 1:
+                        // hubs vão ser ints, representando o número dos vértices? 
+                        int origem, destino;
+                        Console.WriteLine("Informe o hub de origem: ");
+                        origem = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Informe o hub de destino: ");
+                        destino = int.Parse(Console.ReadLine());
+
+                        //chamar método que calcule (caminho mínimo)
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        Console.WriteLine("O programa será encerrado.");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Escolha de 1 a 6.");
+                        break;
+                }
+
+            } while (opc != 6);
         }
     }
 }
